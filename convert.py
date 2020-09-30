1459afd4477022f16182758fdcc04cdcb6ff83f9
Kienyew <aobi100bt@hotmail.com>
2020-09-30 21:22:52 +0800

    Add convert.py

---
+++ convert.py
@@ -0,0 +1,35 @@
+#!python3
+
+import re
+import json
+import argparse
+import unicodedata
+import itertools
+from typing import Iterator, Tuple
+
+from pypinyin import lazy_pinyin
+
+
+
+def pinyin_remove_tone(pinyin: str) -> str:
+    nfkd_form = unicodedata.normalize('NFKD', pinyin)
+    return nfkd_form.encode('ASCII', 'ignore').decode()
+
+
+def main():
+    argument_parser = argparse.ArgumentParser('convert.py', usage='convert.py [JSON_FILE]')
+    argument_parser.add_argument('file', type=argparse.FileType('r'))
+    args = argument_parser.parse_args()
+    idioms = json.load(args.file)
+    results = dict()
+    for entry in idioms:
+        for word in re.split('，|、', entry['word']):
+            pinyin = "'".join(lazy_pinyin(word))
+            results.setdefault(word, pinyin)
+
+    for word, pinyin in sorted(results.items()):
+        print(f'{word}\t{pinyin}\t0')
+
+
+if __name__ == '__main__':
+    main()
